b40aeef0640b1907acaa1c3c1a8ebf8f8156720e
K3;3 <41787377+Wisien999@users.noreply.github.com>
2022-10-30 21:14:03 +0100

    implement strassen

--- lab1/strassen.py
+++ lab1/strassen.py
@@ -1,18 +1,66 @@
 import numpy as np
 
+matrix_t = np.ndarray
 
-def split_into_quarts(matrix: np.array):
+
+def split_into_quarts(matrix: matrix_t) \
+        -> tuple[matrix_t, matrix_t, matrix_t, matrix_t]:
     shape = matrix.shape
     assert shape[0] == shape[1]
     segment_size = shape[0] // 2
     a11 = matrix[0:segment_size, 0:segment_size]
-    a12 = matrix[segment_size:, 0:segment_size]
-    a21 = matrix[0:segment_size, segment_size:]
+    a12 = matrix[0:segment_size, segment_size:]
+    a21 = matrix[segment_size:, 0:segment_size]
     a22 = matrix[segment_size:, segment_size:]
 
     return a11, a12, a21, a22
 
 
+def segment(matrix1: matrix_t, matrix2: matrix_t) \
+        -> tuple[matrix_t, matrix_t, matrix_t, matrix_t, matrix_t, matrix_t, matrix_t]:
+    a11, a12, a21, a22 = split_into_quarts(matrix1)
+    b11, b12, b21, b22 = split_into_quarts(matrix2)
 
-def strassen(matrix1: np.array, matrix2: np.array):
-    pass
+    m1 = strassen((a11 + a22), (b11 + b22))
+    m2 = strassen((a21 + a22), b11)
+    m3 = strassen(a11, (b12 - b22))
+    m4 = strassen(a22, (b21 - b11))
+    m5 = strassen((a11 + a12), b22)
+    m6 = strassen((a21 - a11), (b11 + b12))
+    m7 = strassen((a12 - a22), (b21 + b22))
+
+    return m1, m2, m3, m4, m5, m6, m7
+
+
+def strassen(matrix1: matrix_t, matrix2: matrix_t):
+    assert matrix1.shape == matrix2.shape
+    assert matrix1.shape[0] == matrix1.shape[1]
+
+    if matrix1.shape[0] == 1:
+        return matrix1 * matrix2
+
+    m1, m2, m3, m4, m5, m6, m7 = segment(matrix1, matrix2)
+
+    c11 = m1 + m4 - m5 + m7
+    c12 = m3 + m5
+    c21 = m2 + m4
+    c22 = m1 - m2 + m3 + m6
+
+    upper = np.concatenate((c11, c12), axis=1)
+    lower = np.concatenate((c21, c22), axis=1)
+
+    return np.concatenate((upper, lower), axis=0)
+
+
+if __name__ == '__main__':
+    from numpy.random import rand
+
+    # a = np.array([[1, 2], [3, 4]])
+    # b = np.array([[10, 11], [14, 15]])
+
+    n = 64
+
+    a = rand(n, n)
+    b = rand(n, n)
+
+    print((abs(strassen(a, b) - a@b) < 0.001).all())
